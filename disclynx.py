d952808a29299d394f6750ea3a8f072a20afbc5f
Nate Choe <nate@natechoe.dev>
2022-07-25 06:59:43 +0000

    Split long pages into multiple messages

--- disclynx.py
+++ disclynx.py
@@ -3,6 +3,7 @@ import subprocess
 
 client = discord.Client()
 shebang = '$disclynx '
+max_len = 2000
 
 @client.event
 async def on_ready():
@@ -20,11 +21,22 @@ async def on_message(message):
     output_process = subprocess.run(['lynx', '-dump', url],
             stdout=subprocess.PIPE)
     output_content = output_process.stdout.decode('utf-8')
-    print(output_content)
 
-    await message.channel.send('```\n' + output_content + '\n```')
-    # This naive code block escape strategy is incredibly easy to fool, but I
-    # really don't care.
+    await send_long_message(message.channel, output_content)
+
+async def send_long_message(channel, message):
+    current_message = ""
+    lines = message.split('\n')
+    for line in lines:
+        if len(line) >= 2000:
+            for i in range(0, len(line), 2000):
+                await channel.send(message[i:i + min(2000, len(message - i))])
+            continue
+        elif len(current_message) + len(line) >= 2000:
+            await channel.send(current_message)
+            current_message = ""
+        current_message = current_message + line + '\n'
+    await channel.send(current_message)
 
 secret_file = open("secrets.txt", "r")
 client.run(secret_file.read())
